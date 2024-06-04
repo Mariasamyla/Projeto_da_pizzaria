325389d36ac775c847f0f23e4e03abed3bf84dd0
Mariasamyla <maria.samyla.12@gmail.com>
2024-06-04 08:19:53 -0300

    alterações

---
+++ src/pages/inicio.jsx
@@ -0,0 +1,16 @@
+import {Link, useNavigate} from 'react-router-dom'
+import  logo from '../assets/react.svg'
+import '../App.css'
+ 
+const Home = () => {
+ 
+    return (
+        <div>
+            <h2>Título da página</h2>
+           <p>Outra tela Inicial</p>
+            <img className='img-fluid' src={logo}
+            alt='logo' ></img>
+    </div>
+    )
+    }
+export default Inicio
